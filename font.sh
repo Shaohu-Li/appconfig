ee1cca0f8f3f368f34e5e5e216b8936a2744783d
Shaohu Li <shaohu-li@qq.com>
2025-04-03 01:22:15 +0800

    change Font download

--- font.sh
+++ font.sh
@@ -1,6 +1,10 @@
 # Hack Nerd Font
-cd /tmp
-git clone https://github.com/ryanoasis/nerd-fonts.git --depth 1
-cd nerd-fonts
-chmod +x install.sh
-./install.sh
+# cd /tmp
+# git clone https://github.com/ryanoasis/nerd-fonts.git --depth 1
+# cd nerd-fonts
+# chmod +x install.sh
+# ./install.sh
+
+# Hack Nerd Font
+cd ~/.local/share/fonts && curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/DroidSansMono/DroidSansMNerdFont-Regular.otf
+fc-cache -fv ~/.local/share/fonts
